bdeec8877200a2678a46b75636667a8ded033cbc
Sophie <wanshufen0414@gmail.com>
2023-04-22 23:42:54 +0800

    Website redesign

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,8 @@
 import { defineConfig } from 'astro/config';
-import compress from 'astro-compress';
-import mdx from '@astrojs/mdx';
-import tailwind from '@astrojs/tailwind';
 
-import partytown from "@astrojs/partytown";
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [compress(), mdx(), tailwind(), partytown()]
+  integrations: [tailwind()]
 });
